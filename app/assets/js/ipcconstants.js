de74dbb62a3ff328532faded84700e0816920721
Vin2nt <vincent@samsz.de>
2023-04-01 15:54:44 +0200

    update

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = '70d1d2c1-efd6-44e5-a3bd-a4c7583656ee'
 // SEE NOTE ABOVE.
 
 
